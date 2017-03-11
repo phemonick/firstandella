43dee8c6e08c98f98063b6b86c90fce85af7dd92
omowunmi Elebute <omowunmi Elebute>
2017-03-11 04:22:07 -0500

    solving the text failures

--- minmax_spec.js
+++ minmax_spec.js
@@ -11,11 +11,27 @@ var myApp = require('./minMax.js');
       });
 
       it('should return [4, 6] for [6, 4]', function () {
-        expect(findMinMax([6, 4])).toEqual([4, 6]);
+        expect(myApp.findMinMax([6, 4])).toEqual([4, 6]);
+      });
+
+      it('should return [3, 40] for [6, 4, 20,40]', function () {
+        expect(myApp.findMinMax([6, 4, 20,40])).toEqual([3, 40]);
       });
 
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
-        expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+        expect(myApp.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+      });
+
+      it('should return [1, 99] for [78, 8, 68, 1,89,30,40,99]', function () {
+        expect(findMinMax([78, 8, 68, 1,89,30,40,99])).toEqual([1,99]);
+      });
+
+      it('should return [50,999] for [823,720,540,450,900,1200,900,999,50]', function () {
+        expect(findMinMax([823,720,540,450,900,1200,900,999,50])).toEqual([50,999]);
+      });
+
+      it('should return [8, 200] for [67, 62, 150, 44, 7, 78, 200, 68, 8]', function () {
+        expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([8, 200]);
       });
 
     });
@@ -24,6 +40,14 @@ var myApp = require('./minMax.js');
 
       it('should return [4] for [4, 4, 4, 4]', function () {
         expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+
+      it('should return [10] for [10, 10, 10, 10]', function () {
+        expect(findMinMax([10, 10, 10, 10])).toEqual([10]);
+      });
+
+      it('should return [2] for [2]', function () {
+        expect(findMinMax([2])).toEqual([2]);
       });
 
     });
